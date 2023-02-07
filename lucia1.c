c075dab76b272dd3fa46b8932403555474af7da4
luciariccardi <124668894+luciariccardi@users.noreply.github.com>
2023-02-07 15:13:05 +0100

    Add files via upload

---
+++ lucia1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   printf("inserisci l'éta \n");
+   scanf("%d", &a);
+
+   if (a >= 18)
+   {
+    printf("l'utente e maggiorenne");
+   }
+   
+   else
+   {
+    printf("l'utente e minorenne");
+   }
+
+}
