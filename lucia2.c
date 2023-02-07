633767f68a2d3ca77f66ea3504c37152e50b81da
luciariccardi <124668894+luciariccardi@users.noreply.github.com>
2023-02-07 16:46:50 +0100

    Add files via upload

---
+++ lucia2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+int a;
+int b;
+int c;
+
+printf("inserisci tre numeri \n");
+scanf("%d %d %d" , &a , &b , &c);
+
+if (b - a == c - b) 
+
+
+{
+     
+    printf("possono essere considerati una progressionbe aritmetica");
+  
+    
+}
+ else 
+ {
+    printf("non possono essere considerti una progressione aritmetica");
+ }
+
+
+
+}
