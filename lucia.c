cd80b60d2c33ea19957c72af35f1ddd77fdacf54
luciariccardi <124668894+luciariccardi@users.noreply.github.com>
2023-02-07 11:58:45 +0100

    Add files via upload

--- lucia.c
+++ lucia.c
@@ -2,5 +2,22 @@
 
 int main()
 {
+    int x;
+    int y;
+    int c;
+
+    int divisione =  x / y;
+    int modulo = x % y;
+    int risultato = c;
+    
+    
+  
+    printf("inserisci due numeri: x , y");
+    scanf("leggere i numeri inseriti");
+    printf("la divisione di x e y e: %d\n " , divisione);
+    printf("il modulo di x e y e: %d\n " , modulo);
+    printf("il risultato di x e y è: %d\n" risultato);
+   
+   
 
 } 
