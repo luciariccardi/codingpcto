2da73819b14e9d58d2c7df6024e68246ec25ed5f
luciariccardi <124668894+luciariccardi@users.noreply.github.com>
2023-02-09 11:37:24 +0100

    Add files via upload

--- ll.c
+++ ll.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 int main()
 {
-    int a;
-    int somma = 0;
-    float c = 0;
-     ;
-     while( a >=  0)
-     {   
-        printf("inserisci un numero \n");
-        scanf("%d" , &a);
-        
-     }
-      somma = somma + 1;
-      
-     printf("la media e: %f" , c);
-     
-    
-        printf("%d\n" , somma);
-        c = somma / 2;
-        
+
+
+ float a;
+ int b;
+
+ printf("inserisci due numeri \n");
+ scanf("%f %d", &a , &b);
+ int c = a;
+if(b > 0)
+{
+ while ( b > 1)
+ {
+   
+   b = b - 1;
+   a = c * a;
+
+
+ }
+ 
+
+ printf("la potenza e: %f" , a );
 }
+ else if (b < -1)
+ {
+   while ( b < -1)
+ {
+   
+   b = b + 1;
+   a = 1 / a;
 
 
-
+ }
+  printf("potenza = %f" , a);
+ }
+}
